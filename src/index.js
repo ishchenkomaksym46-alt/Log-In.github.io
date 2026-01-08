41ae2c68ee432715ecdaf7261bd3a633086b0221
Max <ishchenkomaksym46@gmail.com>
2026-01-08 18:49:25 +0200

    Data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const btn = document.getElementById('register')
+
+const data = () => {
+  const name = document.getElementById('name').value 
+  const password = document.getElementById('password').value
+  let data = [name, password]
+
+  console.log(data)
+}
+
+btn.addEventListener('click', data)
